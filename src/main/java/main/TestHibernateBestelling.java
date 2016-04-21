f3d368daa284989d2511cd2b5e0cd9d21534b65c
agung udijana <agung.udijana@gmail.com>
2016-04-21 13:11:24 +0200

    -removed deleteAll from the menu

--- src/main/java/main/TestHibernateBestelling.java
+++ src/main/java/main/TestHibernateBestelling.java
@@ -37,7 +37,6 @@ public class TestHibernateBestelling {
 		System.out.println("3. findById");
 		System.out.println("4. delete");
 		System.out.println("5. findAll");
-		System.out.println("6. deleteAll");
 		System.out.print("Voer optie in en druk op Enter:");
 
 
@@ -370,20 +369,6 @@ public class TestHibernateBestelling {
 				}
 
 				break;
-
-			case 6:
-				System.out.println("*** DeleteAll - start ***");
-
-				System.out.println("Weet u zeker dat u al uw bestelling wilt verwijderen: ");
-				String antwoord = input.next();
-
-				if(antwoord.equalsIgnoreCase("ja")){
-					artikelService.deleteAll();
-					break;
-
-				} else {
-					break;
-				}
 
 			default:
 				System.out.println("\n! Ongeldige optie!\n");
